dd179e7485b0e190e6a71fa51e9b6bb9fe1b2212
naohirooda <perfect9nine0303@icloud.com>
2020-05-04 21:00:59 +0900

    変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -67,27 +67,27 @@ $(function() {
   }
 });
 $('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-    $('.form__submit')[0].reset();
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
   })
-  .fail(function(){
-    alert('error');
-  })
-  always(function(){
-    form__submit.prop('disabled',false);
-  });
-  });
+   .done(function(data){
+     var html = buildHTML(data);
+     $('.messages').append(html);
+     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+     $('.form__submit')[0].reset();
+   })
+   .fail(function(){
+     alert('error');
+   })
+   always(function(){
+     form__submit.prop('disabled',false);
+   });
+   });
